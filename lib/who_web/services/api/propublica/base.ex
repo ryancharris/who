fc101d5419774dec0992164debbbf3667b2cfc81
Ryan Harris <ryan.harris@revzilla.com>
2017-07-29 13:34:04 -0400

    Re-organize ProPublica API service and create a base module the other modules will import

---
+++ lib/who_web/services/api/propublica/base.ex
@@ -0,0 +1,40 @@
+defmodule WhoWeb.APIService.ProPublica.Base do
+  use HTTPoison.Base
+
+  @endpoint "https://api.propublica.org/congress/v1/"
+  @api_key_header ["X-API-KEY": "iKBYwU1Ngx5sKpZpez7eOax7Tr4o7dekaZ4Wz8wu"]
+
+  def get_api_endpoint do
+    @endpoint
+  end
+
+  def get_api_header do
+    @api_key_header
+  end
+
+  @spec make_request(String.t, list()) :: map()
+  @doc """
+    Take in the concatenated API request URL and API key header string, returns result
+  """
+  def make_request(url, api_key) do
+    case get(url, api_key) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        # Return map of decoded JSON blob
+        body |> Poison.decode!
+      {:ok, %HTTPoison.Response{status_code: 404}} ->
+        # When API call fails
+        IO.puts "Not found :("
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        # When an error is returned
+        IO.inspect reason
+    end
+  end
+  
+  @doc """
+    When used, dispatch to the appropriate controller/view/etc.
+  """
+  defmacro __using__(which) when is_atom(which) do
+    apply(__MODULE__, which, [])
+  end
+
+end
---
+++ lib/who_web/services/api/propublica/member.ex
@@ -0,0 +1,18 @@
+defmodule WhoWeb.APIService.ProPublica.Member do
+  @moduledoc """
+    This service defines methods for querying the ProPublica API
+    and parsing the response body
+  """
+  import WhoWeb.APIService.ProPublica.Base
+
+  @spec get_member_by_id(String.t) :: map()
+  @doc """
+      Query the API based on member_id assigned by the Biographical Directory of the United States Congress
+
+      http://bioguide.congress.gov/biosearch/biosearch.asp
+    """
+  def get_member_by_id(member_id) do
+      WhoWeb.APIService.ProPublica.Base.get_api_endpoint() <> "members/#{member_id}.json"
+        |> WhoWeb.APIService.ProPublica.Base.make_request(WhoWeb.APIService.ProPublica.Base.get_api_header())
+  end
+end
