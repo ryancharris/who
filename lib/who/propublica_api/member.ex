fb779ca61ac79cc88e750ee0553e8a7a1d6a7aac
Ryan Harris <ryan.harris@revzilla.com>
2017-08-02 20:57:22 -0400

    Consolidate method definitions

--- lib/who/propublica_api/member.ex
+++ lib/who/propublica_api/member.ex
@@ -21,21 +21,21 @@ defmodule Who.ProPublicaAPI.Member do
 
     EX:
       get_member_by_state("senate", "NJ")
+      get_member_by_state("house", "NJ")
       get_member_by_state("house", "NJ", 12)
   """
   @spec get_member_by_state(String.t(), String.t()) :: map()
+  @spec get_member_by_state(String.t(), String.t(), non_neg_integer()) :: map()
+  @spec get_member_by_state(String.t(), any(), any()) :: map()
   def get_member_by_state(chamber, state) do
     get_api_endpoint() <> "members/#{chamber}/#{state}/current.json"
       |> make_request(get_api_header())
   end
-
-  @spec get_member_by_state(String.t(), String.t(), non_neg_integer()) :: map()
-  def get_member_by_state("senate", _, _) do
-    IO.puts :stderr, ~s(Did you mean "house"? There are no districts in the senate...)
-  end
-
   def get_member_by_state("house", state, district) do
     get_api_endpoint() <> "members/house/#{state}/#{district}/current.json"
       |> make_request(get_api_header())
+  end
+  def get_member_by_state("senate", _, _) do
+    IO.puts :stderr, ~s(Did you mean "house"? There are no districts in the senate...)
   end
 end
