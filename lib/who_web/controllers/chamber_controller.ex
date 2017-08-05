fd2e0f713d9493c7db83fd7be2274096cbe00fa4
Ryan Harris <ryan.harris@revzilla.com>
2017-08-05 11:59:41 -0400

    Pass in recent bills introduced and passed to Chamber show template

--- lib/who_web/controllers/chamber_controller.ex
+++ lib/who_web/controllers/chamber_controller.ex
@@ -1,16 +1,25 @@
 defmodule WhoWeb.ChamberController do
   use WhoWeb, :controller
 
+  alias Who.ProPublicaAPI.Bill
   alias Who.ProPublicaAPI.Chamber
 
   def show(conn, %{"chamber" => chamber}) do
-    [member_list | tail] = Chamber.get_members_by_chamber(chamber)
+    [members | _tail] = Chamber.get_members_by_chamber(chamber)
+    [bills_passed | _tail] = Bill.get_recent_bills(chamber, "passed")
+    [bills_introduced | _tail] = Bill.get_recent_bills(chamber, "introduced")
 
     case chamber do
       "house" ->
-        render conn, "show.html", chamber: "house", members: member_list
+        render conn, "show.html",
+          members: members,
+          bills_passed: bills_passed,
+          bills_introduced: bills_introduced
       "senate" ->
-        render conn, "show.html", chamber: "senate", members: member_list
+        render conn, "show.html",
+          members: members,
+          bills_passed: bills_passed,
+          bills_introduced: bills_introduced
     end
   end
 end
