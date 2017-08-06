54ddd6b0b5614c52e568732266ca060dce42d5d7
Ryan Harris <ryan.harris@revzilla.com>
2017-08-06 19:03:56 -0400

    Update get_recent_bills documentation

--- lib/who/propublica_api/bill.ex
+++ lib/who/propublica_api/bill.ex
@@ -6,6 +6,15 @@ defmodule Who.ProPublicaAPI.Bill do
 
   import Who.ProPublicaAPI.Base
 
+  @doc """
+   Returns a JSON blob representing the last 20 votes in a given
+   chamber of Congress by type.
+
+   EX:
+    Who.ProPublicaAPI.Bill.get_recent_bills("both", "enacted")
+    Who.ProPublicaAPI.Bill.get_recent_bills("senate", "introduced")
+    Who.ProPublicaAPI.Bill.get_recent_bills("house", "passed")
+  """
   @spec get_recent_bills(String.t(), String.t()) :: map()
   def get_recent_bills(chamber, type) do
     get_api_endpoint() <> "115/#{chamber}/bills/#{type}.json"
