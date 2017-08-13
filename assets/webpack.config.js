3757306f8764c2f4c306c36b2f55d3e05b33dadb
Ryan Harris <ryan.harris@revzilla.com>
2017-08-12 22:07:30 -0400

    Transpile JS with babel

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -1,9 +1,19 @@
 const path = require('path');
 
 module.exports = {
-  entry: "../assets/js/app.js",
+  entry: '../assets/js/app.js',
   output: {
-    filename: "app.js",
+    filename: 'app.js',
     path: path.resolve(__dirname, '../priv/static/js')
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015']
+      }
+    }]
   }
 }
