17fff9dc9a6d5815929a31f53c59850c20df2cff
Ryan Harris <ryan.harris@revzilla.com>
2017-08-13 18:20:58 -0400

    Update webpack config

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -2,18 +2,23 @@ const path = require('path');
 
 module.exports = {
   entry: '../assets/js/app.js',
+
   output: {
-    filename: 'app.js',
-    path: path.resolve(__dirname, '../priv/static/js')
+    path: path.resolve(__dirname, '../priv/static/js'),
+    filename: 'app.js'
   },
+
   module: {
-    loaders: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader',
-      query: {
-        presets: ['es2015', 'react']
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, '../assets'),
+        exclude: path.resolve(__dirname, 'node_modules'),
+        loader: 'babel-loader',
+        options: {
+          presets: ['es2015', 'react']
+        }
       }
-    }]
+    ]
   }
 }
