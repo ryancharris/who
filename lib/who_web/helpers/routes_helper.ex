5b87c68cb2d0b58ff6cc57b068ac0e62ea293f41
Ryan Harris <ryan.harris@revzilla.com>
2017-08-18 20:32:46 -0400

    Add documentation for WhoWeb.RoutesHelper.member_id_path

--- lib/who_web/helpers/routes_helper.ex
+++ lib/who_web/helpers/routes_helper.ex
@@ -3,6 +3,14 @@ defmodule WhoWeb.RoutesHelper do
   A set of functions used to build and return custom routes and paths.
   """
 
+  @doc """
+  Creates a relative path to a Member's profile page by ID.
+
+  EX:
+    WhoWeb.RoutesHelper.member_id_path("A000360")
+    >>> /member/A000360
+  """
+  @spec member_id_path(String.t) :: String.t
   def member_id_path(member_id) do
     ~s(/member/#{member_id})
   end
