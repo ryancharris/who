93982046baf9f0ba5f13395169e319773588fff3
Ryan Harris <ryan.harris@revzilla.com>
2017-08-22 20:52:26 -0400

    Add Committee to PropublicaAPI service

---
+++ lib/who/propublica_api/committee.ex
@@ -0,0 +1,8 @@
+defmodule Who.ProPublicaAPI.Committee do
+  @moduledoc """
+  This module defines methods for querying the ProPublica API for information
+  about specific Congressional committees and/or subcommittees
+  """
+  import Who.ProPublicaAPI.Base
+
+end
