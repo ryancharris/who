c3cf6183978ca10dc69959a7aebcb091196901c6
Ryan Harris <ryan.harris@revzilla.com>
2017-08-26 12:42:09 -0400

    Create CommitteePresenter

---
+++ lib/who_web/presenters/committee_presenter.ex
@@ -0,0 +1,8 @@
+defmodule WhoWeb.CommittePresenter do
+  @moduledoc """
+  This module organizes and arranges information regarding a
+  given Committee to reduce logic in the template.
+  """
+  alias WhoWeb.CommitteeView
+
+end
