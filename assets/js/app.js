e30651de0fda548593a6efcb357e1a96ae99049a
Ryan Harris <ryan.harris@revzilla.com>
2017-09-04 20:38:50 -0400

    Add Vue and fix some API call errors

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,3 +9,4 @@ import 'phoenix_html';
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+import Vue from 'vue';
---
+++ assets/js/test.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>test component</div>
+</template>
+<script>
+  export default {
+    props: [],
+    data: {
+      testText: 'fucking test'
+    }
+  }
+</script>
