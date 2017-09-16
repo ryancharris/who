80db660f09eb2b57a60056dc72d866bdc7dee546
Ryan Harris <ryan.harris@revzilla.com>
2017-09-16 14:35:39 -0400

    Add the MemberVotes component to Member #show

---
+++ assets/js/MemberVotes.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="member__votes">
+    <ul>
+      <li v-for="vote in votes">
+        <p v-if="vote.number">{{ vote.number }}</p>
+        <p>{{ vote.description }}</p>
+        <p>{{ vote.position }}</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+    export default {
+        props: ['member'],
+        data() {
+          return {
+            votes: this.member.votes
+          }
+        }
+    }
+</script>
+
+<style>
+</style>
--- assets/js/app.js
+++ assets/js/app.js
@@ -4,10 +4,14 @@ import 'phoenix_html';
 // IMPORT VUE COMPONENTS
 //
 import Vue from 'vue';
+
+
+// Member #show components
 import MemberHeader from './MemberHeader.vue';
 import MemberStats from './MemberStats.vue';
 import MemberMap from './MemberMap.vue';
 import MemberCommittees from './MemberCommittees.vue';
+import MemberVotes from './MemberVotes.vue';
 
 //
 // REGISTER CUSTOM TAGS
@@ -16,6 +20,7 @@ Vue.component('member-header', MemberHeader);
 Vue.component('member-stats', MemberStats);
 Vue.component('member-map', MemberMap);
 Vue.component('member-committees', MemberCommittees);
+Vue.component('member-votes', MemberVotes);
 
 //
 // DECLARE VUE INSTANCE
