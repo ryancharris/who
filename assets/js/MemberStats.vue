6d921381fc345f5d188cf5127bc3820bd75594e3
Ryan Harris <ryan.harris@revzilla.com>
2017-09-16 13:38:04 -0400

    Create MemberStats component

---
+++ assets/js/MemberStats.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="member__stats">
+    <h1 class="state">{{ votes_with_party }}</h1>
+    <h3 class="name">{{ start_date }} {{ end_date }}</h3>
+  </div>
+</template>
+
+<script>
+    export default {
+        props: ['member'],
+        data() {
+          return {
+            votes_with_party: this.member.votes_with_party,
+            start_date: this.member.start_date,
+            end_date: this.member.end_date
+          }
+        }
+    }
+</script>
+
+<style>
+</style>
--- assets/js/app.js
+++ assets/js/app.js
@@ -5,11 +5,13 @@ import 'phoenix_html';
 //
 import Vue from 'vue';
 import MemberHeader from './MemberHeader.vue';
+import MemberStats from './MemberStats.vue';
 
 //
 // REGISTER CUSTOM TAGS
 //
 Vue.component('member-header', MemberHeader);
+Vue.component('member-stats', MemberStats);
 
 //
 // DECLARE VUE INSTANCE
