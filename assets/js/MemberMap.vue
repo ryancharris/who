4a613a8a50510738920edaa62ecfc5235166f774
Ryan Harris <ryan.harris@revzilla.com>
2017-09-16 13:45:57 -0400

    Created MemeberMap component

---
+++ assets/js/MemberMap.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="member__map">
+    <iframe width="100%" height="350" frameborder="0" style="border:0" v-bind:src="url">
+    </iframe>
+  </div>
+</template>
+
+<script>
+    export default {
+        props: ['member'],
+        data() {
+          return {
+            url: this.member.map_src_url
+          }
+        }
+    }
+</script>
+
+<style>
+</style>
--- assets/js/app.js
+++ assets/js/app.js
@@ -6,12 +6,14 @@ import 'phoenix_html';
 import Vue from 'vue';
 import MemberHeader from './MemberHeader.vue';
 import MemberStats from './MemberStats.vue';
+import MemberMap from './MemberMap.vue';
 
 //
 // REGISTER CUSTOM TAGS
 //
 Vue.component('member-header', MemberHeader);
 Vue.component('member-stats', MemberStats);
+Vue.component('member-map', MemberMap);
 
 //
 // DECLARE VUE INSTANCE
