fb7f2cb2d787983d117ebfd45b96635e8f5f83df
Ryan Harris <ryan.harris@revzilla.com>
2017-09-16 13:16:31 -0400

    Update sass-lint

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,20 +3,17 @@ import 'phoenix_html';
 //
 // IMPORT VUE COMPONENTS
 //
-
 import Vue from 'vue';
 import MemberHeader from './MemberHeader.vue';
 
 //
 // REGISTER CUSTOM TAGS
 //
-
 Vue.component('member-header', MemberHeader);
 
 //
 // DECLARE VUE INSTANCE
 //
-
 new Vue({
   el: '#app'
 });
